279fd11143f59832ce1220d8f493d18b0d383810
Hermann Hueck <hermann.hueck@mailbox.org>
2019-09-11 18:28:07 +0200

    utils improved, reformatted

--- util/src/main/scala/util/package.scala
+++ util/src/main/scala/util/package.scala
@@ -1,16 +1,40 @@
 package object util {
 
-  def prtTitle(text: String, width: Int = 80, leading: String = "", trailing: String = "", fill: String = "\u2500"): Unit =
-    println(title(text, width, leading, trailing))
+  def prtTitle(
+      text: String,
+      width: Int = 80,
+      leading: String = "",
+      trailing: String = "",
+      fill: String = "\u2500"
+  ): Unit =
+    println(title(text, width, leading, trailing, fill))
 
-  def title(text: String, width: Int = 80, leading: String = "", trailing: String = "", fill: String = "\u2500"): String = {
+  def title(
+      text: String,
+      width: Int = 80,
+      leading: String = "",
+      trailing: String = "",
+      fill: String = "\u2500"
+  ): String = {
     s"${line(width, leading, "", fill)}${subTitle(text.toUpperCase, width, "\n", s"$trailing\n", fill)}"
   }
 
-  def prtSubTitle(text: String, width: Int = 80, leading: String = "", trailing: String = "", fill: String = "\u2500"): Unit =
-    println(subTitle(text, width, leading, trailing))
+  def prtSubTitle(
+      text: String,
+      width: Int = 80,
+      leading: String = "",
+      trailing: String = "",
+      fill: String = "\u2500"
+  ): Unit =
+    println(subTitle(text, width, leading, trailing, fill))
 
-  def subTitle(text: String, width: Int = 80, leading: String = "", trailing: String = "", fill: String = "\u2500"): String = {
+  def subTitle(
+      text: String,
+      width: Int = 80,
+      leading: String = "",
+      trailing: String = "",
+      fill: String = "\u2500"
+  ): String = {
     val frontPad = fill * 10
     val startLength = (10 + text.length() + 2)
     val endLength = if (startLength > width) 0 else width - startLength
@@ -18,11 +42,21 @@ package object util {
     s"$leading$frontPad $text $endPad$trailing"
   }
 
-  def prtLine(width: Int = 80, leading: String = "", trailing: String = "", fill: String = "\u2500"): Unit =
+  def prtLine(
+      width: Int = 80,
+      leading: String = "",
+      trailing: String = "",
+      fill: String = "\u2500"
+  ): Unit =
     println(line(width, leading, trailing, fill))
 
-  def line(width: Int = 80, leading: String = "", trailing: String = "", fill: String = "\u2500"): String = {
-    val Linea = fill * width
-    s"$leading$Linea$trailing"
-  }  
+  def line(
+      width: Int = 80,
+      leading: String = "",
+      trailing: String = "",
+      fill: String = "\u2500"
+  ): String = {
+    val line = fill * width
+    s"$leading$line$trailing"
+  }
 }
