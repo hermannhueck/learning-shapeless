8873323efe07f71a0a0d817f72b56bd02334ea37
Hermann Hueck <hermann.hueck@mailbox.org>
2019-09-11 17:16:17 +0200

    applied utilities to Chap021Adts

--- gurnell/src/main/scala/chap02/Chap021Adts.scala
+++ gurnell/src/main/scala/chap02/Chap021Adts.scala
@@ -1,10 +1,14 @@
 package chap02
 
+import util._
+
 object Chap021Adts extends App {
 
-  println("\n==== 2.1 Recap: algebraic data types =====")
+  // ----------------------------------------
+  prtTitle("2.1 Recap: algebraic data types")
 
-  println("\n----- Encoding with ADT -----")
+  // ----------------------------------------
+  prtSubTitle("Encoding with ADT")
 
   sealed trait Shape
   final case class Rectangle(width: Double, height: Double) extends Shape
@@ -22,12 +26,12 @@ object Chap021Adts extends App {
   println(area(rect))
   println(area(circ))
 
-
-  println("\n----- Alternative Encoding with Tuples and Either -----")
+  // ----------------------------------------
+  prtSubTitle("Alternative Encoding with Tuples and Either")
 
   type Rectangle2 = (Double, Double)
-  type Circle2    = Double
-  type Shape2     = Either[Rectangle2, Circle2]
+  type Circle2 = Double
+  type Shape2 = Either[Rectangle2, Circle2]
 
   val rect2: Shape2 = Left((3.0, 4.0))
   val circ2: Shape2 = Right(1.0)
@@ -41,5 +45,5 @@ object Chap021Adts extends App {
   println(area2(rect2))
   println(area2(circ2))
 
-  println("==========\n")
+  prtLine()
 }
